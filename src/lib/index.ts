eb92d0490cc9ad83dbac08da8d6cc9bf7ccd855f
Sebastian MC3BCller <sebastian.mueller@red6-es.de>
2018-01-11 17:11:57 +0100

    fix(Module): change provider implementation to fix AOT build issues

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
 import { FeatureFlagsService, IFeatures } from './feature-flags.service';
 import { HideIfFeatureDirective } from './hide-if-feature.directive';
 import { ShowIfFeatureDirective } from './show-if-feature.directive';
@@ -9,25 +9,30 @@ const DIRECTIVES = [
   ShowIfFeatureDirective
 ];
 
+export const FEATURES = new InjectionToken<IFeatures>('FEATURES');
+
+export function featureFlagsServiceFactory(initialState) {
+  return new FeatureFlagsService(initialState);
+}
+
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [
-    ...DIRECTIVES
-  ],
-  exports: [
-    ...DIRECTIVES
-  ]
+  imports: [CommonModule],
+  declarations: DIRECTIVES,
+  exports: DIRECTIVES
 })
 export class NgFeatureFlagsModule {
-  static forRoot(features: IFeatures = {}): ModuleWithProviders {
+  static forRoot(features?: IFeatures): ModuleWithProviders {
     return {
       ngModule: NgFeatureFlagsModule,
       providers: [
         {
+          provide: FEATURES,
+          useValue: features ? features : {}
+        },
+        {
           provide: FeatureFlagsService,
-          useFactory: () => new FeatureFlagsService(features)
+          useFactory: featureFlagsServiceFactory,
+          deps: [FEATURES]
         }
       ]
     };
